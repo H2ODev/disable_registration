194e4d917f892ae795f063c6129fb736ea33ec65
Marvin Oßwald <mail@marvinosswald.de>
2015-09-26 10:15:59 -0400

    Applied fixes from StyleCI

--- src/Model/Observer.php
+++ src/Model/Observer.php
@@ -1,22 +1,28 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: marvinosswald
  * Date: 25.09.15
- * Time: 11:18
+ * Time: 11:18.
  */
 class H2ODev_DisableRegistration_Model_Observer
 {
     public function __construct()
     {
     }
+
     /**
-     * Exports a given MagentoOrderExport
-     * @param   Varien_Event_Observer $observer
+     * Exports a given MagentoOrderExport.
+     *
+     * @param Varien_Event_Observer $observer
+     *
      * @return true
      */
-    public function disable ($observer){
+    public function disable($observer)
+    {
         $observer->result->setIsAllowed(false);
+
         return true;
     }
 }
